0a64f0c74d689bf54b1530340441ed197ac76dbb
PammyLydia <64384456+PammyLydia@users.noreply.github.com>
2020-05-03 15:26:48 +1000

    Update opentable_paralbar

--- opentable_paralbar.py
+++ opentable_paralbar.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("state_of_industry_data.csv")
 countries = data.loc[0:8].iloc[:, 2:52].T
 dates = data.iloc[:, 2:52].columns
-x = list(range(50))
 width = 0.1
 plt.figure(figsize=(25, 10))
 for i in range(1, 9):
@@ -12,7 +11,7 @@ for i in range(1, 9):
     for j in range(len(x)):
         x[j] = x[j] + width*(i-1)
 
-    plt.bar(x, height=countries[i-1], width=width, label=data.loc[i-1]["Name"],tick_label = dates)
+    plt.bar(x, height=countries[i-1], width=width, label=data.loc[i-1]["Name"], tick_label=dates)
     plt.legend()
 
 plt.xticks(rotation=45)
